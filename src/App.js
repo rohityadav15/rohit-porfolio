e8af6ff0bcfa924ceb86041753dd91ba2916c33b
Rohit Yadav <rohit.yadav102002@gmail.com>
2026-03-01 12:31:45 +0530

    Logo and Image

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import Home from './Home';
+
+const App=()=> {
+   return (
+    <Box>
+      <Home />
+      <h1>Welcome to My Portfolio</h1>
+    </Box>
+  );
+}
+export default App;  
