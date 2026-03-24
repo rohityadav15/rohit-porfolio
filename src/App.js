f835378947445a4154ef295dc8a80766f63347ad
Rohit Yadav <rohit.yadav102002@gmail.com>
2026-03-24 20:40:20 +0530

    main file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { Box } from '@mui/material';
-import Home from './Home';
+import { Box } from "@mui/material";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Component/Header";
 
-const App=()=> {
-   return (
-    <Box>
-      <Home />
-      <h1>Welcome to My Portfolio</h1>
-    </Box>
+const App = () => {
+  return (
+    <Router>
+      {/* <Home /> */}
+      <Header />
+      <Box>
+        <h1>Welcome on Home page</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Box>
+    </Router>
   );
-}
-export default App;  
+};
+export default App;
